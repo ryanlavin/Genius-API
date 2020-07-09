2a4bacc99d9ca6217313e430b7502b7d015c601f
Ryan Lavin <rlavin@usc.edu>
2020-07-09 15:12:35 -0500

    Next to do is db work w lyrics

--- backend/user-auth.go
+++ backend/user-auth.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"github.com/gorilla/mux"
 	//"github.com/sirupsen/logrus"
 	//"html/template"
@@ -9,6 +9,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"golang.org/x/crypto/bcrypt"
 	"net/http"
+	"net/url"
 )
 
 var db *sql.DB
@@ -23,6 +24,10 @@ type Handler struct {
 }
 
 func Orig(res http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" {
+		Register(res, req)
+		return
+	}
 	http.ServeFile(res, req, "../frontend/index.html")
 }
 
@@ -48,7 +53,6 @@ func Login(res http.ResponseWriter, req *http.Request) {
 }
 
 func Register(res http.ResponseWriter, req *http.Request) {
-
 	if req.Method != "POST" {
 		http.ServeFile(res, req, "../frontend/register.html")
 		return
@@ -56,7 +60,6 @@ func Register(res http.ResponseWriter, req *http.Request) {
 
 	username = req.FormValue("username")
 	password = req.FormValue("password")
-	fmt.Println(username, password)
 	err = db.QueryRow("SELECT username FROM user WHERE username=?", username).Scan(&user)
 	if err == sql.ErrNoRows {
 		cryptedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -79,7 +82,40 @@ func Register(res http.ResponseWriter, req *http.Request) {
 }
 
 func Homepage(res http.ResponseWriter, req *http.Request) {
+	if err != nil {
+		panic(err.Error())
+	}
 	http.ServeFile(res, req, "../frontend/homepage.html")
+}
+
+func PullLyrics(res http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		http.ServeFile(res, req, "../frontend/pullLyrics.html")
+		return
+	}
+	artist := req.FormValue("artist")
+	title := req.FormValue("title")
+
+
+	if err != nil {
+		panic(err.Error())
+	}
+	req, err = http.NewRequest("GET", "https://api.lyrics.ovh/v1/artist/title", nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	//q := req.URL.Query()
+	q := url.Values{}
+	q.Add("artist", artist)
+	q.Add("title", title)
+	//fmt.Println(q.Get("artist"))
+	//fmt.Println(q.Get("title"))
+	//fmt.Println(req.URL.RawQuery)
+	http.ServeFile(res, req, "../frontend/pullLyrics.html")
+}
+
+func SavedLyrics(res http.ResponseWriter, req *http.Request) {
+	http.ServeFile(res, req, "../frontend/savedLyrics.html")
 }
 
 func main() {
@@ -98,9 +134,10 @@ func main() {
 	router.HandleFunc("/login", Login)
 	router.HandleFunc("/register", Register)
 	router.HandleFunc("/homepage", Homepage)
-	err = http.ListenAndServe(":8080", router)
+	router.HandleFunc("/pulllyrics", PullLyrics)
+	router.HandleFunc("/savedlyrics", SavedLyrics)
+	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		panic(err)
 	}
-
 }
